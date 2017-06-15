b23d836b958f04e2bda0e1a36aeac461d77e4919
krisvanneste <kris.vanneste@oma.be>
2017-06-15 22:06:29 +0000

    Updated test_sensitivity.py script to the various changes in the functions to compute and plot rupture probabilities.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4216 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- test_sensitivity.py
+++ test_sensitivity.py
@@ -11,6 +11,7 @@ gis_folder = os.path.join(project_folder, "GIS")
 
 
 ## Scenarios
+#scenario = "Quitralco"
 scenario = "Azul Tigre South"
 #scenario = "Due East"
 
@@ -31,8 +32,12 @@ ipe_name = "BakunWentworth1997WithSigma"
 #ipe_name = "AllenEtAl2012"
 trt_gsim_dict = {TRT: ipe_name}
 ground_motion_model = rshalib.gsim.GroundMotionModel(ipe_name, trt_gsim_dict)
-gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF([ipe_name], [1])}
-integration_distance = 300
+#gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF([ipe_name], [1])}
+gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF(["BakunWentworth1997WithSigma", "AllenEtAl2012", "AtkinsonWald2007"], [0.6, 0.3, 0.1])}
+integration_distance_dict = {"AtkinsonWald2007": (None, 30)}
+#TODO: apply distance filtering for BakunWentworth1997WithSigma when M larger than
+#threshold causing too high values in near field (to be determined)
+#integration_distance_dict = {"AtkinsonWald2007": (None, 30), "BakunWentworth1997WithSigma": (20, None)}
 truncation_level = 2.5
 #truncation_level = 0.
 #if "AllenEtAl2012" in ipe_name:
@@ -78,7 +83,7 @@ for site_model in all_site_models:
 	dsha_model = rshalib.shamodel.DSHAModel(scenario, flt_src_model, gmpe_system_def,
 				grid_outline=[], grid_spacing=None,
 				soil_site_model=site_model, imt_periods={"MMI": [0]},
-				truncation_level=0, integration_distance=integration_distance)
+				truncation_level=0, integration_distance=500)
 
 	uhs_field = dsha_model.calc_gmf_fixed_epsilon()
 	hm = uhs_field.getHazardMap()
@@ -99,8 +104,9 @@ print avg_prob
 ## Compute probability for this scenario
 scenario_src_model = rshalib.source.SourceModel(scenario, [flt])
 prob_dict = calc_rupture_probability_from_ground_motion_thresholds(
-					scenario_src_model, ground_motion_model, imt, pe_site_models,
+					scenario_src_model, gmpe_system_def, imt, pe_site_models,
 					pe_thresholds, ne_site_models, ne_thresholds, truncation_level,
+					integration_distance_dict=integration_distance_dict,
 					strict_intersection=strict_intersection)
 [scenario_prob] = prob_dict.values()[0]
 print scenario_prob
@@ -110,14 +116,12 @@ print scenario_prob
 dM = 0.5
 min_mag, max_mag = 6.0 - dM/2, 7.0
 #Mrange = np.arange(min_mag, max_mag, dM) + dM/2
-#Mrange = np.linspace(flt.mfd.min_mag - dM, flt.mfd.min_mag + dM, 3)
-Mrange = [flt.mfd.min_mag]
+Mrange = np.linspace(flt.mfd.min_mag - dM, flt.mfd.min_mag + dM, 3)
+#Mrange = [flt.mfd.min_mag]
 print Mrange
 
 
 ## Map parameters
-#max_prob_color = 1.0
-max_prob_color = None
 #map_region = (-74, -72, -46, -44.5)
 map_region = (-74, -72, -46.25, -44.75)
 output_format = "png"
@@ -137,8 +141,9 @@ for M in Mrange:
 
 	## Compute rupture probabilities
 	prob_dict = calc_rupture_probability_from_ground_motion_thresholds(
-						source_model, ground_motion_model, imt, pe_site_models,
+						source_model, gmpe_system_def, imt, pe_site_models,
 						pe_thresholds, ne_site_models, ne_thresholds, truncation_level,
+						integration_distance_dict=integration_distance_dict,
 						strict_intersection=strict_intersection)
 
 	#for rup_name in prob_dict:
@@ -164,5 +169,5 @@ for M in Mrange:
 	fig_filespec = None
 
 	plot_rupture_probabilities(source_model, prob_dict, pe_site_models, ne_site_models,
-								map_region, avg_prob, max_prob_color, plot_point_ruptures=True,
+								map_region, plot_point_ruptures=True,
 								title=title, text_box=text_box, fig_filespec=fig_filespec)
