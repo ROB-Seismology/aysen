0386e67041e7931254bff2ef59692a05dba13f08
krisvanneste <kris.vanneste@oma.be>
2017-07-28 14:59:46 +0000

    Added magnitude to scenario rupture labels.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4288 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- plot_scenarios_and_sites.py
+++ plot_scenarios_and_sites.py
@@ -1,11 +1,12 @@
 
 import os
 import mapping.layeredbasemap as lbm
+from hazard.rshalib.source.read_from_gis import import_source_model_from_gis
 from rupture_probabilities import *
 
 
-project_folder = r"C:\Users\kris\Documents\Publications\2017 - Aysen"
-#project_folder = r"E:\Home\_kris\Publications\2017 - Aysen"
+#project_folder = r"C:\Users\kris\Documents\Publications\2017 - Aysen"
+project_folder = r"E:\Home\_kris\Publications\2017 - Aysen"
 gis_folder = os.path.join(project_folder, "GIS")
 fig_folder = os.path.join(project_folder, "Figures")
 
@@ -16,8 +17,8 @@ map_region = (-74, -72, -46.0, -44.75)
 output_format = "png"
 
 fig_filename = "Scenario_and_site_map.%s" % output_format
-#fig_filespec = os.path.join(fig_folder, fig_filename)
-fig_filespec = None
+fig_filespec = os.path.join(fig_folder, fig_filename)
+#fig_filespec = None
 
 
 layers = []
@@ -39,9 +40,18 @@ layers.append(layer)
 
 ## Rupture scenarios
 gis_filespec = os.path.join(gis_folder, 'LOFZ_rupture_scenarios.TAB')
-data = lbm.GisData(gis_filespec, label_colname='Name')
+src_model = import_source_model_from_gis(gis_filespec, column_map={'id': 'Name'},
+					dip=90, rake=180, lower_seismogenic_depth=12.5, upper_seismogenic_depth=0)
+labels = {}
+for flt in src_model:
+	mag = flt.calc_Mmax_Wells_Coppersmith()
+	label = "%s\n(M=%.1f)" % (flt.source_id, mag)
+	labels[flt.source_id] = label
+print labels
+joined_attributes = {'label': {'key': 'Name', 'values': labels}}
+data = lbm.GisData(gis_filespec, label_colname='label', joined_attributes=joined_attributes)
 data.style_params['label_anchor'] = [0., 0., 1., None, None]
-label_style = lbm.TextStyle(font_size=10, color='m', rotation='auto', vertical_alignment='center', horizontal_alignment='center', offset=(5,-5))
+label_style = lbm.TextStyle(font_size=9, color='m', rotation='auto', vertical_alignment='center', horizontal_alignment='center', offset=(8,-8))
 style = lbm.LineStyle(line_color='m', line_width=2, label_style=label_style, label_anchor=0.5)
 layer = lbm.MapLayer(data, style, legend_label='Test scenarios')
 layers.append(layer)
@@ -64,8 +74,8 @@ for polygon in polygons[1:]:
 print site_names
 data.style_params['label_anchor'] = ['east', None, None, 'south', 'south', None]
 data.style_params['horizontal_alignment'] = ['left', None, None, 'right', 'left', None]
-data.style_params['offset'] = [(3,0), None, None, (0,0), (0,0), None]
-label_style = lbm.TextStyle(font_size=10, color='b', horizontal_alignment='right', offset=(-3,0))
+data.style_params['offset'] = [(3,0), None, None, (0,0), (3,0), None]
+label_style = lbm.TextStyle(font_size=9, color='b', horizontal_alignment='right', offset=(-3,0))
 style = lbm.PolygonStyle(line_color=None, line_width=0, fill_color='b', label_style=label_style, label_anchor="west", alpha=0.5)
 layer = lbm.MapLayer(data, style, legend_label='Polygon sites')
 layers.append(layer)
@@ -83,7 +93,7 @@ for point_data in multipoint_data:
 data = points[0].to_multi_point()
 for point in points[1:]:
 	data.append(point)
-label_style = lbm.TextStyle(font_size=10, color='b', horizontal_alignment='left', offset=(7,0))
+label_style = lbm.TextStyle(font_size=9, color='b', horizontal_alignment='left', offset=(7,0))
 style = lbm.PointStyle(shape='D', size=6, fill_color='b', line_color='k', label_style=label_style)
 layer = lbm.MapLayer(data, style, legend_label='Point sites')
 layers.append(layer)
