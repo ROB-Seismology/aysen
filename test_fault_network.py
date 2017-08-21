9811fe565ca0d4fac0c9118cd40f427cb9b88c5b
krisvanneste <kris.vanneste@oma.be>
2017-08-21 15:11:40 +0000

    Further work on test_fault_network.py.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4302 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- test_fault_network.py
+++ test_fault_network.py
@@ -3,15 +3,15 @@ import hazard.rshalib as rshalib
 from rupture_probabilities import *
 
 
-project_folder = r"C:\Users\kris\Documents\Publications\2017 - Aysen"
-#project_folder = r"E:\Home\_kris\Publications\2017 - Aysen"
+#project_folder = r"C:\Users\kris\Documents\Publications\2017 - Aysen"
+project_folder = r"E:\Home\_kris\Publications\2017 - Aysen"
 gis_folder = os.path.join(project_folder, "GIS")
 
 gis_filename = "LOFZ_breukenmodel3.TAB"
 gis_filespec = os.path.join(gis_folder, gis_filename)
 
 allow_triple_junctions = False
-rms = 5.7
+rms = 2.85
 src_model = read_fault_source_model(gis_filespec, characteristic=False)
 for flt in src_model:
 	flt.rupture_mesh_spacing = rms
@@ -20,17 +20,31 @@ for flt in src_model:
 flt_network = src_model.get_fault_network(allow_triple_junctions=allow_triple_junctions)
 flt_network.check_consistency()
 print flt_network.fault_links['0#09']
-#exit()
+
+src_model.get_linked_subfaults([['1#02', '1#01', '12#29']])
+exit()
+
 #connections = flt_network.get_connections('0#01', 25, allow_triple_junctions=allow_triple_junctions)
-connections = flt_network.get_all_connections(35, allow_triple_junctions=allow_triple_junctions)
+connections = flt_network.get_all_connections(66, allow_triple_junctions=allow_triple_junctions)
 print len(connections), max([len(conn) for conn in connections])
+faults = []
+mags = {}
 for conn in connections:
-	if len(conn) == 32:
+	if len(conn) == 3:
 	#if '8' in [subflt_id.split('#')[0] for subflt_id in conn]:
-		print conn
+		#print conn
+		fault_model = src_model.get_linked_subfaults([conn])
+		[flt] = fault_model.sources
+		faults.append(flt)
+		#mag = flt.calc_Mmax_Wells_Coppersmith()
+		#num_sections = len(conn)
+		#if not num_sections in mags:
+		#	mags[num_sections] = mag
 
-		faults = src_model.get_linked_subfaults([conn])
+#pylab.plot(mags.keys(), mags.values())
+#pylab.show()
 
-map = faults.get_plot()
+exit()
+fault_model = rshalib.source.SourceModel("", faults)
+map = fault_model.get_plot()
 map.plot()
-
