0f94752a5499afcfa458a07c2ab9175e133bca24
krisvanneste <kris.vanneste@oma.be>
2017-10-09 13:17:41 +0000

    Added create_animated_gif.py to create gif animations from different image sequences.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4342 40b490c5-b4d9-47cb-8714-9bef99b524d5

---
+++ create_animated_gif.py
@@ -0,0 +1,35 @@
+import os
+import imageio
+
+
+def create_animated_gif(folder, img_basename, ext=".png", duration=1):
+	images = []
+	for file in os.listdir(folder):
+		if (file[:len(img_basename)] == img_basename and
+			os.path.splitext(file)[1] in (ext, ext.upper())):
+			img_filespec = os.path.join(folder, file)
+			img = imageio.imread(img_filespec)
+			images.append(img)
+
+	out_file = os.path.join(folder, img_basename + ".gif")
+	imageio.mimsave(out_file, images, duration=duration, subrectangles=True)
+
+
+folder = r"E:\Home\_kris\Publications\2017 - Aysen\Figures\Sensitivity\v4"
+
+ipe_names = ["Barrientos1980WithSigma", "BakunWentworth1997WithSigma", "AllenEtAl2012", "AtkinsonWald2007"]
+
+## Resolution power figures
+for ipe_name in ipe_names:
+	img_basename = "ResPow_%s" % ipe_name
+	#print img_basename
+	#create_animated_gif(folder, img_basename)
+
+## Scenario sensitivity figures
+scenarios = ["Quitralco", "Azul Tigre South", "2007", "Due East", "Due West"]
+for scenario in scenarios:
+	for mmi in ["7.5", "mix"]:
+		for ipe_name in ["BakunWentworth1997WithSigma", "LogicTree"]:
+			img_basename = "%s_MMI=%s_%s" % (scenario, mmi, ipe_name)
+			#print img_basename
+			#create_animated_gif(folder, img_basename)
