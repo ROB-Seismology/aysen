cb79d2a5c959307eefbe56ab5bbcbdc18ab8f8d7
krisvanneste <kris.vanneste@oma.be>
2018-02-13 13:36:49 +0000

    Added __main__ section in script.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4492 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- create_animated_gif.py
+++ create_animated_gif.py
@@ -15,23 +15,24 @@ def create_animated_gif(folder, img_basename, ext=".png", duration=1):
 	imageio.mimsave(out_file, images, duration=duration, subrectangles=True)
 
 
-folder = r"E:\Home\_kris\Publications\2017 - Aysen\Figures\Sensitivity\v4"
+if __name__ == "__main__":
+	folder = r"E:\Home\_kris\Publications\2017 - Aysen\Figures\Sensitivity\v4"
 
-ipe_names = ["Barrientos1980WithSigma", "BakunWentworth1997WithSigma", "AllenEtAl2012", "AtkinsonWald2007"]
+	ipe_names = ["Barrientos1980WithSigma", "BakunWentworth1997WithSigma", "AllenEtAl2012", "AtkinsonWald2007"]
 
-## Resolution power figures
-for ipe_name in ipe_names:
-	img_basename = "ResPow_%s" % ipe_name
-	#print img_basename
-	#create_animated_gif(folder, img_basename)
+	## Resolution power figures
+	for ipe_name in ipe_names:
+		img_basename = "ResPow_%s" % ipe_name
+		#print img_basename
+		#create_animated_gif(folder, img_basename)
 
-## Scenario sensitivity figures
-#scenarios = ["Quitralco", "Azul Tigre South", "2007", "Due East", "Due West"]
-scenarios = ["Quitralco"]
-for scenario in scenarios:
-	for mmi in ["6.5", "9"]:
-		#for ipe_name in ["BakunWentworth1997WithSigma", "LogicTree"]:
-		for ipe_name in ["BakunWentworth1997WithSigma"]:
-			img_basename = "%s_MMI=%s_%s" % (scenario, mmi, ipe_name)
-			print img_basename
-			create_animated_gif(folder, img_basename)
+	## Scenario sensitivity figures
+	#scenarios = ["Quitralco", "Azul Tigre South", "2007", "Due East", "Due West"]
+	scenarios = ["Quitralco"]
+	for scenario in scenarios:
+		for mmi in ["6.5", "9"]:
+			#for ipe_name in ["BakunWentworth1997WithSigma", "LogicTree"]:
+			for ipe_name in ["BakunWentworth1997WithSigma"]:
+				img_basename = "%s_MMI=%s_%s" % (scenario, mmi, ipe_name)
+				print img_basename
+				create_animated_gif(folder, img_basename)
