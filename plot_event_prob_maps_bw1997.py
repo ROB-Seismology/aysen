584a701eaf33080ae978b7849ba08abd67bb75c1
krisvanneste <kris.vanneste@oma.be>
2018-02-13 13:36:00 +0000

    Improved reading of MTD evidence.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4491 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- plot_event_prob_maps_bw1997.py
+++ plot_event_prob_maps_bw1997.py
@@ -12,14 +12,17 @@ from aysenlib import (create_uniform_grid_source_model, plot_gridsearch_map,
 						TRT, USD, LSD, RAR, RMS, project_folder, gis_folder)
 
 
+fig_folder = os.path.join(project_folder, "Figures", "Events", "bw1997", "NE=+1.0")
+
 
 ## Event names
 events = ['2007', 'SL-A', 'SL-B', 'SL-C', 'SL-CD', 'SL-D', 'SL-DE', 'SL-EF', 'SL-F', 'SL-G']
-events = ["SL-C"]
+#events = ["SL-C"]
 #events = events[:1]
 
 ## IPE and IMT
 ipe_name = "BakunWentworth1997"
+#ipe_name = "AtkinsonWald2007"
 imt = oqhazlib.imt.MMI()
 
 polygon_discretization = 2.5
@@ -29,35 +32,46 @@ polygon_discretization = 2.5
 grid_outline = (-74, -72, -46.25, -44.75)
 grid_spacing = 0.1
 
-min_mag, max_mag, mag_bin_width = 4.5, 7.5, 0.2
+min_mag, max_mag, mag_bin_width = 4.5, 8.5, 0.2
 depth = 10
-strike, dip, rake = 90, 60, 0
+strike, dip, rake = 35, 80, 180
 point_msr = oqhazlib.scalerel.PointMSR()
+wc1994_msr = oqhazlib.scalerel.WC1994()
 
 grd_src_model = SimpleUniformGridSourceModel(grid_outline, grid_spacing,
 			min_mag, min_mag + mag_bin_width, mag_bin_width, depth,
-			strike, dip, rake, point_msr, USD, LSD, RMS, RAR, TRT)
+			strike, dip, rake, wc1994_msr, USD, LSD, RMS, RAR, TRT)
 lon_grid, lat_grid = grd_src_model.lon_grid, grd_src_model.lat_grid
 
 
 ## Loop over events
 for event in events:
 	## Read MTD evidence
+	pe_site_models, ne_site_models = [], []
 	pe_thresholds, pe_sites, ne_thresholds, ne_sites = [], [], [], []
 	for geom_type in ["Polygons", "Points"]:
 		shapefile = os.path.join(gis_folder, "%s.shp" % geom_type)
 		(_pe_thresholds, _pe_site_models,
-		_ne_thresholds, _ne_site_models) = read_evidence_site_info_from_gis(shapefile, event, polygon_discretization)
+		_ne_thresholds, _ne_site_models) = read_evidence_site_info_from_gis(shapefile,
+													event, polygon_discretization)
+		pe_site_models.extend(_pe_site_models)
+		ne_site_models.extend(_ne_site_models)
 		for pesm, pe_threshold in zip(_pe_site_models, _pe_thresholds):
-			pe_sites.extend(pesm.get_sites())
-			pe_thresholds.extend([pe_threshold] * len(pesm))
-		pe_site_model = rshalib.site.SoilSiteModel("Positive evidence", pe_sites)
+			for pe_site in pesm.get_sites():
+				pe_sites.append(pe_site)
+				pe_thresholds.append(pe_threshold)
+				print("+%s: %s" % (pe_site.name, pe_threshold))
 		for nesm, ne_threshold in zip(_ne_site_models, _ne_thresholds):
-			ne_sites.extend(nesm.get_sites())
-			ne_thresholds.extend([ne_threshold] * len(nesm))
-		ne_site_model = rshalib.site.SoilSiteModel("Negative evidence", ne_sites)
-		print len(pe_sites), len(ne_sites)
-		print len(pe_thresholds), len(ne_thresholds)
+			for ne_site in nesm.get_sites():
+				ne_sites.append(ne_site)
+				ne_thresholds.append(ne_threshold)
+				print("-%s: %s" % (ne_site.name, ne_threshold))
+		print sum([len(pesm) for pesm in _pe_site_models]), sum([len(nesm) for nesm in _ne_site_models])
+	print len(pe_sites), len(ne_sites)
+	pe_thresholds = np.array(pe_thresholds)
+	ne_thresholds = np.array(ne_thresholds)
+
+	#ne_thresholds -= 0.5
 
 	## Compute magnitudes and RMS errors at grid points
 	(mag_grid, rms_grid) = (
@@ -67,14 +81,20 @@ for event in events:
 	idx = np.unravel_index(rms_grid.argmin(), rms_grid.shape)
 	print mag_grid[idx], lon_grid[idx], lat_grid[idx]
 
+	rms_grid[np.isnan(mag_grid)] = np.inf
+
 
 	## Plot map
+	# TODO: blend alpha in function of rms
+	# See: https://matplotlib.org/devdocs/gallery/images_contours_and_fields/image_transparency_blend.html
+	site_model_gis_file = os.path.join(gis_folder, "Polygons.shp")
 	map = plot_gridsearch_map(grd_src_model, mag_grid, rms_grid,
-							[pe_site_model], [ne_site_model])
+							pe_site_models, ne_site_models,
+							site_model_gis_file=site_model_gis_file,
+							plot_rms_as_alpha=False, plot_epicenter_as="both")
 
-	point_data = lbm.PointData(lon_grid[idx], lat_grid[idx])
-	point_style = lbm.PointStyle(shape='*', fill_color='yellow', size=14)
-	layer = lbm.MapLayer(point_data, point_style)
-	map.layers.append(layer)
+	fig_filespec = os.path.join(fig_folder, "%s_bw1997.png" % event)
+	#fig_filespec = None
 
-	map.plot()
+	dpi = 200 if fig_filespec else 90
+	map.plot(fig_filespec=fig_filespec, dpi=dpi)
