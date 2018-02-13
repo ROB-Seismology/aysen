8170bc6ee952da3ace8e66b95b0f115c86b66ffd
krisvanneste <kris.vanneste@oma.be>
2018-02-13 13:31:12 +0000

    Moved calculation of fault networks outside of the loop. Added generation of GIF animation.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/hazard/psha/trunk/Projects/Aysen_fjord@4490 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- plot_event_prob_maps.py
+++ plot_event_prob_maps.py
@@ -1,23 +1,25 @@
 import os
 import numpy as np
+
 import hazard.rshalib as rshalib
+
 from aysenlib import *
+from create_animated_gif import create_animated_gif
 
 
 # TODO: check if negative evidence can be more strict (higher or lower probabilities?): event SL-C
 
 from aysenlib import (project_folder, gis_folder)
-
+base_fig_folder = os.path.join(project_folder, "Figures", "Events", "v2", "NE=+1.0")
 
 ## Event names
 events = ['2007', 'SL-A', 'SL-B', 'SL-C', 'SL-CD', 'SL-D', 'SL-DE', 'SL-EF', 'SL-F', 'SL-G']
-#events = ["SL-C"]
-events = events[:1]
+events = ["SL-A"]
+#events = events[2:]
 
 
 ## IPE names
 #ipe_names = ["LogicTree", "AllenEtAl2012", "AtkinsonWald2007", "BakunWentworth1997WithSigma", "Barrientos1980WithSigma"]
-#ipe_names = ipe_names[3:4]
 ipe_names = ["BakunWentworth1997WithSigma"]
 
 
@@ -44,112 +46,125 @@ output_format = "png"
 """
 dM = 0.25
 min_mag, max_mag = 6.0 - dM/2, 7.0
-Mrange = np.arange(min_mag, max_mag, dM) + dM/2
-print Mrange
-for M in Mrange:
+fault_mags = np.arange(min_mag, max_mag, dM) + dM/2
+print fault_mags
+for M in fault_mags:
 	#fault_filespec = os.path.join(gis_folder, "LOFZ_breukenmodel.shp")
 	fault_filespec = os.path.join(gis_folder, "LOFZ_breukenmodel2.TAB")
 	source_model = read_fault_source_model_as_floating_ruptures(fault_filespec, M, M + dM/2, dM, depth=0.1)
 """
 
-## Fault network
+## Discretize faults as network
+fault_filespec = os.path.join(gis_folder, "LOFZ_breukenmodel3.TAB")
 dM = 0.2
-Mrange = []
+fault_mags, fault_networks = [], []
+for M, source_model in read_fault_source_model_as_network(fault_filespec, dM=dM):
+	fault_mags.append(M)
+	fault_networks.append(source_model)
+
+
+## Loop over events
 max_prob_dict = {}
 section_prob_dict = {}
-fault_filespec = os.path.join(gis_folder, "LOFZ_breukenmodel3.TAB")
-for M, source_model in read_fault_source_model_as_network(fault_filespec, dM=dM):
-	Mrange.append(M)
+for event in events:
+	print event
+	max_prob_dict[event] = {}
+	section_prob_dict[event] = {}
 
-	## Loop over events
-	for event in events:
-		if M == Mrange[0]:
-			max_prob_dict[event] = {}
-			section_prob_dict[event] = {}
-		fig_folder = os.path.join(project_folder, "Figures", "Events", "v2", event)
-		if not os.path.exists(fig_folder):
-			os.mkdir(fig_folder)
+	fig_folder = os.path.join(base_fig_folder, event)
+	if not os.path.exists(fig_folder):
+		os.mkdir(fig_folder)
 
-		## Read MTD evidence
-		pe_thresholds, pe_site_models, ne_thresholds, ne_site_models = [], [], [], []
-		for geom_type in ["Polygons", "Points"]:
-			shapefile = os.path.join(gis_folder, "%s.shp" % geom_type)
-			(_pe_thresholds, _pe_site_models,
-			_ne_thresholds, _ne_site_models) = read_evidence_site_info_from_gis(shapefile, event, polygon_discretization)
-			pe_thresholds.extend(_pe_thresholds)
-			pe_site_models.extend(_pe_site_models)
-			ne_thresholds.extend(_ne_thresholds)
-			ne_site_models.extend(_ne_site_models)
+	## Read MTD evidence
+	pe_thresholds, pe_site_models, ne_thresholds, ne_site_models = [], [], [], []
+	for geom_type in ["Polygons", "Points"]:
+		shapefile = os.path.join(gis_folder, "%s.shp" % geom_type)
+		(_pe_thresholds, _pe_site_models,
+		_ne_thresholds, _ne_site_models) = read_evidence_site_info_from_gis(shapefile, event, polygon_discretization)
+		pe_thresholds.extend(_pe_thresholds)
+		pe_site_models.extend(_pe_site_models)
+		ne_thresholds.extend(_ne_thresholds)
+		ne_site_models.extend(_ne_site_models)
 
-		intensity_correction = 0
-		#if ipe_name == "AllenEtAl2012Rrup":
-		#	intensity_correction = -0.5
-		#elif ipe_name == "AtkinsonWald2007":
-		#	intensity_correction = -1
-		pe_thresholds = np.array(pe_thresholds) + intensity_correction
-		ne_thresholds = np.array(ne_thresholds) + intensity_correction
+	intensity_correction = 0
+	#if ipe_name == "AllenEtAl2012Rrup":
+	#	intensity_correction = -0.5
+	#elif ipe_name == "AtkinsonWald2007":
+	#	intensity_correction = -1
+	pe_thresholds = np.array(pe_thresholds) + intensity_correction
+	ne_thresholds = np.array(ne_thresholds) + intensity_correction
 
-		print pe_thresholds
-		#ne_thresholds = np.minimum(7.5, ne_thresholds)
-		print ne_thresholds
+	#ne_thresholds -= 1.0
 
-		## Construct ground-motion model
-		for ipe_name in ipe_names:
-			if M == Mrange[0]:
-				max_prob_dict[event][ipe_name] = []
-				section_prob_dict[event][ipe_name] = {}
-			if ipe_name != "LogicTree":
-				trt_gsim_dict = {TRT: ipe_name}
-				gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF([ipe_name], [1])}
-				integration_distance_dict = {}
-			else:
-				models = ["Barrientos1980WithSigma", "BakunWentworth1997WithSigma", "AllenEtAl2012", "AtkinsonWald2007"]
-				weights = [0.32, 0.26, 0.25, 0.17]
-				gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF(models, weights)}
-				integration_distance_dict = {"AtkinsonWald2007": (None, 30)}
+	for pe_site_model, pe_threshold in zip(pe_site_models, pe_thresholds):
+		print("+%s (n=%d): %s" % (pe_site_model.name.encode(errors='replace'), len(pe_site_model), pe_threshold))
+	for ne_site_model, ne_threshold in zip(ne_site_models, ne_thresholds):
+		print("-%s (n=%d): %s" % (ne_site_model.name.encode(errors='replace'), len(ne_site_model), ne_threshold))
 
 
-			## Compute rupture probabilities
-			prob_dict = calc_rupture_probability_from_ground_motion_thresholds(
-								source_model, gmpe_system_def, imt, pe_site_models,
-								pe_thresholds, ne_site_models, ne_thresholds, truncation_level,
-								integration_distance_dict=integration_distance_dict,
-								strict_intersection=strict_intersection)
-			#print prob_dict
-			probs = np.array(prob_dict.values())
-			probs = probs[:, 0]
-			max_prob = probs.max()
-			max_prob_dict[event][ipe_name].append(max_prob)
-			print M, max_prob
-			for rup_name, prob in zip(prob_dict.keys(), probs):
-				for section in rup_name.split('+'):
-					if not section in section_prob_dict[event][ipe_name]:
-						section_prob_dict[event][ipe_name][section] = [prob]
-					else:
-						section_prob_dict[event][ipe_name][section].append(prob)
+	## Construct ground-motion model
+	for ipe_name in ipe_names:
+		max_prob_dict[event][ipe_name] = []
+		section_prob_dict[event][ipe_name] = {}
+		if ipe_name != "LogicTree":
+			trt_gsim_dict = {TRT: ipe_name}
+			gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF([ipe_name], [1])}
+			integration_distance_dict = {}
+		else:
+			models = ["Barrientos1980WithSigma", "BakunWentworth1997WithSigma", "AllenEtAl2012", "AtkinsonWald2007"]
+			weights = [0.32, 0.26, 0.25, 0.17]
+			gmpe_system_def = {TRT: rshalib.pmf.GMPEPMF(models, weights)}
+			integration_distance_dict = {"AtkinsonWald2007": (None, 30)}
 
-			## Plot
-			if "WithSigma" in ipe_name:
-				ipe_label = ipe_name[:ipe_name.find("WithSigma")]
-			else:
-				ipe_label = ipe_name
+	for M, source_model in zip(fault_mags, fault_networks):
+		## Compute rupture probabilities
+		prob_dict = calc_rupture_probability_from_ground_motion_thresholds(
+							source_model, gmpe_system_def, imt, pe_site_models,
+							pe_thresholds, ne_site_models, ne_thresholds, truncation_level,
+							integration_distance_dict=integration_distance_dict,
+							strict_intersection=strict_intersection)
+		#print prob_dict
+		probs = np.array(prob_dict.values())
+		probs = probs[:, 0]
+		max_prob = probs.max()
+		max_prob_dict[event][ipe_name].append(max_prob)
+		print M, max_prob
+		for rup_name, prob in zip(prob_dict.keys(), probs):
+			for section in rup_name.split('+'):
+				if not section in section_prob_dict[event][ipe_name]:
+					section_prob_dict[event][ipe_name][section] = [prob]
+				else:
+					section_prob_dict[event][ipe_name][section].append(prob)
 
-			text_box = "Event: %s\nIPE: %s\nM: %.2f, Pmax: %.2f"
-			text_box %= (event, ipe_label, M, max_prob)
+		## Plot
+		if "WithSigma" in ipe_name:
+			ipe_label = ipe_name[:ipe_name.find("WithSigma")]
+		else:
+			ipe_label = ipe_name
 
-			#title = "Event: %s, IPE: %s, M=%.2f" % (event, ipe_name, M)
-			title = ""
+		text_box = "Event: %s\nIPE: %s\nM: %.2f, Pmax: %.2f"
+		text_box %= (event, ipe_label, M, max_prob)
 
-			fig_filename = "%s_%s_M=%.2f.%s" % (event, ipe_label, M, output_format)
-			fig_filespec = os.path.join(fig_folder, fig_filename)
-			#fig_filespec = None
+		#title = "Event: %s, IPE: %s, M=%.2f" % (event, ipe_name, M)
+		title = ""
 
-			## Colormaps: RdBu_r, YlOrRd, BuPu, RdYlBu_r, Greys
-			site_model_gis_file = os.path.join(gis_folder, "Polygons.shp")
-			plot_rupture_probabilities(source_model, prob_dict, pe_site_models, ne_site_models,
-										map_region, plot_point_ruptures=True, colormap="RdYlBu_r",
-										title=title, text_box=text_box, site_model_gis_file=site_model_gis_file,
-										fig_filespec=fig_filespec)
+		fig_filename = "%s_%s_M=%.2f.%s" % (event, ipe_label, M, output_format)
+		fig_filespec = os.path.join(fig_folder, fig_filename)
+		#fig_filespec = None
+
+		## Colormaps: RdBu_r, YlOrRd, BuPu, RdYlBu_r, Greys
+		site_model_gis_file = os.path.join(gis_folder, "Polygons.shp")
+		plot_rupture_probabilities(source_model, prob_dict, pe_site_models, ne_site_models,
+									map_region, plot_point_ruptures=True, colormap="RdYlBu_r",
+									title=title, text_box=text_box, site_model_gis_file=site_model_gis_file,
+									fig_filespec=fig_filespec)
+
+
+	## Generate animated GIF
+	for ipe_name in ipe_names:
+		img_basename = "%s_%s" % (event, ipe_label)
+		create_animated_gif(fig_folder, img_basename)
+
 
 ## Determine which sections have highest probability
 for event in events:
@@ -169,20 +184,21 @@ for event in events:
 ## Plot max_prob vs magnitude for different IPEs
 colors = ['r', 'b', 'g', 'm', 'k']
 for event in events:
+	pylab.cla()
 	for ipe_name, color in zip(ipe_names, colors):
 		if "WithSigma" in ipe_name:
 			label = ipe_name[:ipe_name.find("WithSigma")]
 		else:
 			label = ipe_name
-		pylab.plot(Mrange, max_prob_dict[event][ipe_name], 'x-', color=color, label=label)
-	pylab.xlim(Mrange[0], Mrange[-1])
+		pylab.plot(fault_mags, max_prob_dict[event][ipe_name], 'x-', color=color, label=label)
+	pylab.xlim(fault_mags[0], fault_mags[-1])
 	pylab.ylim(0, 1)
 	pylab.xlabel("Magnitude")
-	pylab.ylabel("Probability")
+	pylab.ylabel("Max. normalized probability")
 	pylab.title("Event: %s" % event)
 	pylab.legend(loc=3)
 
-	fig_folder = os.path.join(project_folder, "Figures", "Events", "v2", event)
+	fig_folder = os.path.join(base_fig_folder, event)
 	fig_filename = "%s_M_vs_prob.%s" % (event, output_format)
 	fig_filespec = os.path.join(fig_folder, fig_filename)
 	#fig_filespec = None
